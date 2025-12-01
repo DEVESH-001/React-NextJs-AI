791aa2950e22b02f5a04feadf711722691dd6371
DEVESH-001 <dd4168@srmist.edu.in>
2025-12-01 14:04:19 +0530

    Mini_project_1

---
+++ reactjs/4-mini-project/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+import QueueForm from "./components/QueueForm";
+import QueueDisplay from "./components/QueueDisplay";
+
+function App() {
+  const [queue, setQueue] = useState([]);
+
+  const addToQueue = (customer) => {
+    //add data to queue
+    setQueue([...queue, { ...customer, id: Date.now(), status: "waiting" }]);
+  };
+
+  const updateStatus = (id, newStatus) => {
+    //change data to queue
+    setQueue(
+      queue.map((customer) =>
+        customer.id === id ? { ...customer, status: newStatus } : customer
+      )
+    );
+  };
+
+  const removeFromQueue = (id) => {
+    //remove data from queue
+    setQueue(queue.filter((customer) => customer.id !== id));
+  };
+
+  return (
+    <div className="app">
+      <header>
+        <h1>Queue Management System</h1>
+        <p>Manage your customer efficently</p>
+      </header>
+      <main>
+        <QueueForm onAdd={addToQueue} />
+        <QueueDisplay
+          queue={queue}
+          onUpdateStatus={updateStatus}
+          onRemove={removeFromQueue}
+        />
+      </main>
+    </div>
+  );
+}
+
+export default App;
